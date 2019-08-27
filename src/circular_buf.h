acb57e4a1814f596488923208631dc8bacf288eb
Steven Daglish <s.c.daglish@gmail.com>
2019-08-27 07:47:56 +0100

    Further information provide on create function

--- src/circular_buf.h
+++ src/circular_buf.h
@@ -25,6 +25,32 @@ typedef struct circular_buf_struct {
 
 typedef struct circular_buf_struct circular_buf_t;
 
+/**
+ * @brief   Returns a pointer to a circular buffer.
+ *
+ * This function is unable to dynamically create the array and circular buffer.
+ * Therefore it is necessary for these to already be created and their pointers
+ * to be passed in this function.
+ *
+ * The function will then add the address of the array into the circular
+ * buffer's struct for later use.
+ *
+ * Example:
+ * @code
+ * const uint8_t buf_size = 8;
+ * circular_buf_t circular_buf;
+ * circular_buf_t * p__circular_buf = &circular_buf;
+ * uint8_t p_buf_array[buf_size];
+ *
+ * p_circular_buf = circular_buf_create(buf_size, p_buf_array, p_circular_buf);
+ * @endcode
+ *
+ * @param   capacity        Maximum size of the circular buffer (max 256)
+ * @param   p_buf_array     Pointer to already created uint8_t array
+ * @param   p_circular_buf  Pointer to circular buffer (this pointer will be
+ * used to create the circular buffer)
+ * @return  circular_buf_t* Pointer to the created circular buffer
+ */
 circular_buf_t *circular_buf_create(uint8_t capacity, uint8_t *p_buf_array,
                                     circular_buf_t *p_circular_buf);
 
